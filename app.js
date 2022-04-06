841919bd0d954602b2987da29ed9431c7ec0b4d3
Julien <julien.sebag@me.com>
2022-04-06 23:44:42 +0200

    Added deploy code

--- app.js
+++ app.js
@@ -43,6 +43,14 @@ app.use("/conversations", conversations)
 const search = require("./routes/search")
 app.use("/search", search)
 
+// Deploy Heroku
+const path = require("path")
+app.use(express.static(path.join(__dirname, "/client/build")))
+app.use((req, res) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/client/build/index.html")
+})
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app)
 
